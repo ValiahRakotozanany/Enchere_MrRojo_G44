b5099409c8c0cd1949e72481d941f607778b4ea9
ValiahRakotozanany <99878610+ValiahRakotozanany@users.noreply.github.com>
2023-02-03 17:12:53 +0300

    Update PhotoController.java

--- src/main/java/com/example/demo/controller/PhotoController.java
+++ src/main/java/com/example/demo/controller/PhotoController.java
@@ -23,27 +23,32 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author Murphy
  */
-
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/Enchere/photo")
 public class PhotoController {
-    
+
     @Autowired
     PhotoService photoService;
-    
+
     TokenUtilisateur tokenutilisateur;
 
-    
     @PostMapping
-    public ResponseEntity save (@RequestBody Photo photo, HttpServletRequest request) throws Exception {
+    public ResponseEntity save(@RequestBody Photo photo, HttpServletRequest request) throws Exception {
         //tokenutilisateur.verifierTokenClient(token, request);
         System.out.println(" mandeee");
-        HashMap<String,Object> resultat = new HashMap<>();
-        Photo p= photoService.save(photo);
-        resultat.put("data",p);
-        System.out.println(" mandeee be");
-        return new ResponseEntity(resultat,HttpStatus.CREATED);
+        HashMap<String, Object> resultat = new HashMap<>();
+
+        try {
+
+            Photo p = photoService.save(photo);
+            resultat.put("data", p);
+            System.out.println(" mandeee be");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity(resultat, HttpStatus.CREATED);
+
     }
-    
+
 }
