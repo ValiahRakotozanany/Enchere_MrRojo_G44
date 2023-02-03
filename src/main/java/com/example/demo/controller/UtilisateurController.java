b81fdcaebf944ddecc3311f66968d47a1e6adcc9
ValiahRakotozanany <99878610+ValiahRakotozanany@users.noreply.github.com>
2023-02-03 16:20:58 +0300

    col

--- src/main/java/com/example/demo/controller/UtilisateurController.java
+++ src/main/java/com/example/demo/controller/UtilisateurController.java
@@ -56,7 +56,7 @@ public class UtilisateurController {
     TokenUtilisateur tokenutilisateur;
     
     @PostMapping("insertion")
-    public ResponseEntity<Utilisateur> saveUtilisateur(@RequestBody Utilisateur utilisateur, HttpServletRequest request) throws Exception {        
+    public ResponseEntity<Utilisateur> saveUtilisateur(@RequestBody Utilisateur utilisateur, HttpServletRequest request) throws Exception {    
         return new ResponseEntity<Utilisateur>(utilisateurService.saveUtilisateur(utilisateur), HttpStatus.CREATED);
     }
     
